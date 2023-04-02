785bc99053dcfe5497776781f4390cdba4437e72
timebreaker49 <wcsbaylor@gmail.com>
2023-04-02 08:11:35 -0500

    first commit in authentication repo

---
+++ backend/authentication/views.py
@@ -0,0 +1,32 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class HomeView(APIView):
+    
+    permission_classes = (IsAuthenticated, )
+    
+    def get(self, request):
+        content = {
+            'message': 'Welcome to the JWT Authentication page with Django + React!'
+        }
+        
+        return Response(content)        
+
+class LogoutView(APIView):
+    
+    permission_classes = (IsAuthenticated, )
+    
+    def post(self, request):
+        print(request.data)
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            
+            return Response(status=status.HTTP_400_BAD_REQUEST)
