f80d850b84676aca7c2f544c075622a84f7b091c
timebreaker49 <wcsbaylor@gmail.com>
2023-04-12 10:10:36 -0500

    adding python-dotenv to manage env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# dotenv path
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
